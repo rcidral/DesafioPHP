4df6b09c02b4b05246c50347fcd6a741fd1c0898
rcidral <rcidralmfo@gmail.com>
2023-04-17 09:53:32 -0300

    fix bugs

--- App/Views/Produto/index.php
+++ App/Views/Produto/index.php
@@ -57,7 +57,7 @@
         <div class="product-incorret">
             <p>Desculpe não encontramos o item procurado</p>
         </div>
-        <?php if($_SESSION['produto_sequencia1'] == "" && $_SESSION['produto_sequencia2'] == "" && $_SESSION['produto_sequencia3'] == "" ) { ?>
+        <?php if($_SESSION['produto_sequencia1'] != "" || $_SESSION['produto_sequencia2'] != "" || $_SESSION['produto_sequencia3'] != "" ) { ?>
         <div class="container-carrousel-2">
             <div class="carousel-wrapper">
                 <div class="carousel-container">
